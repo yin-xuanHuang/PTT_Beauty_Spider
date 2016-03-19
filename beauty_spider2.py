2419a3d26f117b5ee44dd3034399ac1fac8e5101
twtrubiks <twtrubiks@gmail.com>
2016-03-19 13:08:49 +0800

    add board , image format and Improve performance

--- beauty_spider2.py
+++ beauty_spider2.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
+import download_beauty
 import requests
 import sys
-import download_beauty
+import time 
+from time import strftime
 from bs4 import BeautifulSoup
-from time import time 
-from time import  strftime
 requests.packages.urllib3.disable_warnings()
 
-
-rs=requests.session()
+rs = requests.session()
+Borad = ''
+article_list = []
+load = {
+'from':'/bbs/'+Borad+'/index.html',
+'yes':'yes' 
+}
     
 def getPageNumber(content) :
     startIndex = content.find('index')
@@ -16,62 +21,111 @@ def getPageNumber(content) :
     pageNumber = content[startIndex+5 : endIndex]
     return pageNumber
 
-def crawPage(url, article_list, push_rate):
-    res=rs.get(url,verify=False)
-    soup=BeautifulSoup(res.text,'html.parser')   
+def over18(board):
+    res = rs.get('https://www.ptt.cc/bbs/' + board + '/index.html', verify = False)
+    #先檢查網址是否包含'over18'字串 ,如有則為18禁網站
+    if ( res.url.find('over18') > -1 ):
+       print u"18禁網頁"
+       load = {
+           'from':'/bbs/'+board+'/index.html',
+           'yes':'yes' 
+       }
+       res = rs.post('https://www.ptt.cc/ask/over18',verify = False, data = load)
+       return  BeautifulSoup(res.text,'html.parser')  
+    return BeautifulSoup(res.text,'html.parser')  
 
-    for r_ent in soup.select('div.r-ent'):
+def crawPage(url, push_rate):
+    res = rs.get(url, verify = False)
+    soup = BeautifulSoup(res.text,'html.parser')   
+    for r_ent in soup.find_all( class_="r-ent"):
         try:
-            atag=r_ent.select('div.title')[0].find('a')
-            if(atag):
-                URL=atag['href']   
-                title=r_ent.select('div.title')[0].text.strip()
-                URL='https://www.ptt.cc'+URL
-                rate = r_ent.select('div.nrec')[0].text
-    
-                if(rate):       
-                   comment_rate = rate          
-                   if rate == u'爆':
-                     comment_rate = 100
-                   if rate[0] == 'X':
-                     comment_rate =  -1 * int(rate[1])          
-                else:
-                   comment_rate = 0
-
-                if int(comment_rate) >= push_rate:
-                   article_list.append((int(comment_rate), URL, title))		                            
+           #先得到每篇文章的篇url
+           link = r_ent.find('a')['href']
+           comment_rate = ""
+           if ( link ) :
+               #確定得到url再去抓 標題 以及 推文數
+               title = r_ent.find(class_="title").text.strip()            
+               rate = r_ent.find(class_="nrec").text
+               URL ='https://www.ptt.cc' + link   
+               if(rate):       
+                  comment_rate = rate          
+                  if rate.find(u'爆') > -1:
+                    comment_rate = 100
+                  if rate.find('X') > -1:
+                    comment_rate =  -1 * int(rate[1])          
+               else:
+                  comment_rate = 0
+               #比對推文數
+               if int(comment_rate) >= push_rate:
+                  article_list.append( (int(comment_rate), URL, title) )       	                            
         except:
-            print 'error:',URL
+           #print u'crawPage function error:',r_ent.find(class_="title").text.strip() 
+           print u'本文已被刪除'
               
    
 if __name__ == '__main__':
-    start_page, page_term, push_rate = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]) 
-    ts = time() 
-    CrawlerTime= "BeautyPicture_"+strftime("%Y-%m-%d[%H-%M-%S]")
+    #python beauty_spider2.py [版名] [爬蟲起始的頁面] [爬幾頁] [推文多少以上] 
+    Borad, start_page, page_term, push_rate = sys.argv[1],int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]) 
+    start_time = time.time()
+    CrawlerTime = "_PttImg_"+strftime("%Y%m%d%H%M%S")
+    if start_page == 0:
+       print u"請輸入有效數字"
+       sys.exit()
+    
+    # 如為 -1 ,則從最新的一頁開始
     if start_page < 0:
-        res=rs.get('https://www.ptt.cc/bbs/Beauty/index.html',verify=False)
-        soup=BeautifulSoup(res.text,'html.parser')    
+        #檢查看板是否為18禁,有些看板為18禁
+        soup = over18(Borad) 
         ALLpageURL = soup.select('.btn.wide')[1]['href']
-        ALLpage = start_page = int(getPageNumber(ALLpageURL))+1      
+        ALLpage = start_page = int(getPageNumber(ALLpageURL)) + 1      
 
-
-    print "Analytical download page..."
-
-    article_list = []
+    print u"Analytical download page..."
+    index_list = []
     for page in range(start_page, start_page - page_term, -1):
-        page_url = 'https://www.ptt.cc/bbs/Beauty/index' + str(page) + '.html'
-        crawPage(page_url, article_list, push_rate)
-
+        page_url = 'https://www.ptt.cc/bbs/' + Borad + '/index' + str(page) + '.html'
+        index_list.append(page_url)
+    
+    #抓取 文章標題 網址 推文數
+    while index_list:
+        index = index_list.pop(0)
+        res = rs.get( index, verify = False )
+        soup = BeautifulSoup(res.text,'html.parser')   
+        #如網頁忙線中,則先將網頁加入 index_list 並休息1秒後再連接
+        if (soup.title.text.find('Service Temporarily') > -1) :
+            index_list.append(index)
+            #print u'error_URL:',index
+            time.sleep(1)
+        else : 
+            crawPage(index, push_rate)
+            #print u'OK_URL:', index 
+        time.sleep(0.05)
+    
     total = len(article_list)
     count = 0
-    for hot_rate, url, title in article_list:
-        download_beauty.store_pic(CrawlerTime, url, str(hot_rate),title)
-        count += 1
-        print "download: " + str(100 * count / total ) + " %."
+    #進入每篇文章分析內容
+    while article_list:
+          article = article_list.pop(0)
+          # url = article[1] 
+          res = rs.get( article[1], verify = False )
+          soup = BeautifulSoup(res.text,'html.parser')
+          #如網頁忙線中,則先將網頁加入 index_list 並休息1秒後再連接
+          if (soup.title.text.find('Service Temporarily') > -1) :
+            article_list.append(article)
+            #print u'error_URL:',article[1]
+            time.sleep(1)
+          else : 
+            count += 1
+            #print u'OK_URL:', article[1]
+            # rate = article[0], url = article[1], title = article[2]
+            # store_pic(CrawlerTime, url, rate="", title="" ):
+            download_beauty.store_pic(CrawlerTime, article[1], str(article[0]), article[2])
+            print u"download: " + str(100 * count / total ) + " %."
+          time.sleep(0.05)
 
-    print "DONE"
-    print('Time {}s'.format(time() - ts)) 
-
+    print u"下載完畢..."
+    print u"execution time:" + str(time.time() - start_time)+"s"
+    
+    
 
 
 
--- download_beauty.py
+++ download_beauty.py
@@ -10,54 +10,62 @@ import sys
 import requests
 import shutil
 import multiprocessing
-from time import time 
+import time 
+from time import  strftime
 from bs4 import BeautifulSoup
 from functools import partial
-from time import  strftime
 requests.packages.urllib3.disable_warnings()
 
-rs=requests.session()
-
+rs = requests.session()
+#移除特殊字元（移除Windows上無法作為資料夾的字元）
 def remove(value, deletechars):
     for c in deletechars:
         value = value.replace(c,'')
     return value.rstrip();
 
+#符合圖片格式的網址
+def isImageFormat(link):
+    if(link.find('.jpg') > -1 or link.find('.png') > -1 or link.find('.gif') > -1 or link.find('.jpeg') > -1):
+       return True;
+    return False;
 
 def store_pic(CrawlerTime, url, rate="", title="" ):
-    res=rs.get(url,verify=False)
-    soup=BeautifulSoup(res.text,'html.parser')      
+    #檢查看板是否為18禁,有些看板為18禁
+    soup = over18(url)  
+    CrawlerTime = url.split('/')[-2] + CrawlerTime
+    #避免有些文章會被使用者自行刪除標題列  
     if(title == ""):
        try:
-          title= soup.select('.article-meta-value')[2].text
+          title = soup.select('.article-meta-value')[2].text
        except:
-          title= "no title"
-
+          title = "no title"
+    
     dir_name = remove(title, '\/:*?"<>|.') + "_"+ rate 
-    pic_url_list=[]
+    pic_url_list = []
 
+    #抓取圖片URL(img tag )
     for img in soup.select('img'):
-        if (img['src'].find('.jpg')>0 or img['src'].find('.png')>0):
-            if (img['src'].find('http')==-1 ):
-               link= 'http:'+ img['src'] 
+        if ( isImageFormat(img['src']) ):
+            if (img['src'].find('http') == -1 ):
+               link = 'http:'+ img['src'] 
             else:
-               link= img['src']    
+               link = img['src']    
             pic_url_list.append(link)
 
-    if(len(pic_url_list)!=0):
+    #開始建立資料夾,使用文章標題做為資料夾的名稱
+    if( len(pic_url_list) != 0):
         relative_path = os.path.join(CrawlerTime,dir_name)
-        path= os.path.abspath(relative_path) 
+        path = os.path.abspath(relative_path) 
         try:    
            if not os.path.exists(path):                    
               os.makedirs(path)              
         except Exception, e: 
-           print 'os.makedirs(path) error'  
+           print u'os.makedirs(path) error'  
   
-        pool_size = multiprocessing.cpu_count()*2        
+        pool_size = multiprocessing.cpu_count() * 2        
         download = partial(download_link, relative_path)      
-        pool = multiprocessing.Pool(processes=pool_size, 
-                                    #initializer=start_process, 
-                                    #maxtasksperchild=4, 
+        pool = multiprocessing.Pool(processes = pool_size, 
+
         ) 
         
         pool_outputs = pool.map(download, pic_url_list) 
@@ -65,43 +73,73 @@ def store_pic(CrawlerTime, url, rate="", title="" ):
         pool.join() 
  
 def download_link(directory, link): 
-    resImg=rs.get(link, stream=True,verify=False)  
-    filename= link.split('/')[-1]              
+    resImg = rs.get(link, stream = True,verify = False) 
+    #使用網址的最後一個字串設為圖片檔案名稱 
+    filename = link.split('/')[-1]              
     relative_path = os.path.join(directory,filename)
-    path= os.path.abspath(relative_path)
+    path = os.path.abspath(relative_path)
     try:   
         if not os.path.exists(path): 
            with open(path, 'wb') as out_file: 
                 shutil.copyfileobj(resImg.raw, out_file) 
            del resImg         
     except Exception, e: 
-        print 'shutil.copyfileobj error'     
-       
+        print u'shutil.copyfileobj error'     
+   
+def over18(url):
+    res = rs.get(url, verify = False)
+    #先檢查網址是否包含'over18'字串 ,如有則為18禁網站
+    if ( res.url.find('over18') > -1 ):
+       print u"18禁網頁"
+       #從網址獲得版名
+       Board = url.split('/')[-2]
+       load = {
+           'from':'/bbs/'+Board+'/index.html',
+           'yes':'yes' 
+       }
+       res = rs.post('https://www.ptt.cc/ask/over18',verify = False, data = load)
+       return  BeautifulSoup(res.text,'html.parser')  
+    return BeautifulSoup(res.text,'html.parser')  
         
 def main():
-    print "Crawler Parsing...."
-    CrawlerTime= "BeautyPicture_"+strftime("%Y-%m-%d[%H-%M-%S]")
-    ts = time() 
+    print u"Analytical download page..."
+    CrawlerTime = "_PttImg_"+strftime("%Y%m%d%H%M%S")
+    start_time = time.time()
     beauty_article_urls = []
-    # 從檔案中讀入 urls
-    total=0
+    # 從.txt檔案中讀取 urls
+    total = 0
     with open(sys.argv[1]) as fd:
         for url in fd:
-            beauty_article_urls.append(url.strip())
-            total+=1
+            if( url.strip().find("www.ptt.cc") > -1 ):
+               beauty_article_urls.append(url.strip())
+               total += 1
             
-    count=0
-    for article_url in beauty_article_urls:
-        # 下載該網頁的圖片
-        count+=1
-        print "Crawling: " + str(100 * count / total ) + " %."
-        store_pic(CrawlerTime,article_url)
-        
-    print('Time {}s'.format(time() - ts)) 
-    print "Crawler End...."
+    count = 0
+    while beauty_article_urls :
+          URL = beauty_article_urls.pop(0)
+          #檢查看板是否為18禁,有些看板為18禁
+          soup = over18(URL)
+          #如網頁忙線中,則先將網頁加入 index_list 並休息1秒後再連接
+          if (soup.title.text.find('Service Temporarily') > -1) :
+              beauty_article_urls.append(URL);
+              #print u'error_URL:',URL
+              time.sleep(1)
+          else : 
+              #print u'OK_URL:', URL 
+              # 下載該網頁的圖片
+              count += 1       
+              store_pic(CrawlerTime, URL)
+              print u"Crawling: " + str(100 * count / total ) + " %."
+          time.sleep(0.05)
+          
+    print u"下載完畢..."
+    print u"execution time:" + str(time.time() - start_time)+"s"
 
 if __name__ == '__main__':
     main()
+
+
+
 
 
 
